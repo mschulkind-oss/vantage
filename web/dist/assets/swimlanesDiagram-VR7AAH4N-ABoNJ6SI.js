c52de3c0bcfca35d6d2b4f42eb79f2b484f78022
Matthew Schulkind <mschulkind@gmail.com>
2026-09-03 12:22:15 -0400

    release v0.5.7

---
+++ web/dist/assets/swimlanesDiagram-VR7AAH4N-ABoNJ6SI.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-B6xuSHsQ.js";import"./chunk-DU6HZSFF-GCOHH42x.js";import"./chunk-75Z2AOVW-D-jc8gzT.js";import"./chunk-PWAF6VOD-Cfyw4HqR.js";import"./chunk-GMAD6QVW-C9lkC_fy.js";import"./chunk-P2QGCYS3-B_eAcs53.js";import"./chunk-4HAMMTFA-BahSUBuz.js";import"./chunk-GVQU2GXP-DN1x76By.js";import"./chunk-OSK3NFVY-DAWB-ePh.js";import"./chunk-F27PBJKO-cDU5yIiJ.js";import"./chunk-XXDRQBXY-DqAYeljN.js";import"./chunk-POPQ4Y6H-BdGRI7jV.js";import"./chunk-L3NEJ4N5-D9rZN0DJ.js";import{r as t,t as n}from"./chunk-SHT3W25Y-CK-T82jK.js";import"./mermaid.core-DKJJUQ0u.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
