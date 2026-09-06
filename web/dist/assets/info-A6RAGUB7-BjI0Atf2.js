d77f265735d4e8381ecdb3fc997d9f754888757a
Matthew Schulkind <mschulkind@gmail.com>
2026-09-05 23:06:17 -0400

    release v0.5.9

---
+++ web/dist/assets/info-A6RAGUB7-BjI0Atf2.js
@@ -0,0 +1,1 @@
+import"./chunk-FOHPRMQF-DHwB1DNv.js";import{w as e}from"./mermaid-parser.core-kaTWEv81.js";export{e as createInfoServices};
---
+++ web/dist/assets/swimlanesDiagram-VR7AAH4N-D82_UiBP.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-B6xuSHsQ.js";import"./chunk-DU6HZSFF-nGp0eOnW.js";import"./chunk-75Z2AOVW-25uF0aPn.js";import"./chunk-PWAF6VOD-DfvLLMkT.js";import"./chunk-GMAD6QVW-BwHskUbt.js";import"./chunk-P2QGCYS3-D5BQmBbF.js";import"./chunk-4HAMMTFA-C7r3DJ8Y.js";import"./chunk-GVQU2GXP-DibMkjum.js";import"./chunk-OSK3NFVY-BSDt9r5v.js";import"./chunk-F27PBJKO-DQ03GUAZ.js";import"./chunk-XXDRQBXY-DqAYeljN.js";import"./chunk-POPQ4Y6H-BWcj4swg.js";import"./chunk-L3NEJ4N5-QxFCjmYh.js";import{r as t,t as n}from"./chunk-SHT3W25Y-CyHEYkRB.js";import"./mermaid.core-DYQhn6Dp.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
